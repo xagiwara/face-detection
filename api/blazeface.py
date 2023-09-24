c6c22e4c11673e9c19de1ab350cb2c182913d3ca
m1nt-s0da <m1nt-s0da@qxoxp.xyz>
2023-09-25 01:50:31 +0900

    :pencil2: 訓練済みモデルを読み込んだ後キャッシュする

--- api/blazeface.py
+++ api/blazeface.py
@@ -1,5 +1,5 @@
 from .app import app
-from fastapi import UploadFile
+from fastapi import UploadFile, Query
 import numpy as np
 import cv2
 from blazeface.blazeface import BlazeFace
@@ -7,11 +7,43 @@ from typing import Optional
 from env import DATA_BLAZEFACE
 from os.path import join
 from device import cudaDevice
+from time import perf_counter
 
+model_cached: dict[str, BlazeFace] = {}
+
+def load_model(model: str, device: str):
+    key = '%s/%s' % (model, device)
+
+    if key in model_cached:
+        return model_cached[key]
+
+    if model == 'front':
+        blazeface = BlazeFace().to(device)
+        blazeface.load_weights(join(DATA_BLAZEFACE, 'blazeface.pth'))
+        blazeface.load_anchors(join(DATA_BLAZEFACE, 'anchors.npy'))
+
+    if model == 'back':
+        blazeface = BlazeFace(back_model=True).to(device)
+        blazeface.load_weights(join(DATA_BLAZEFACE, 'blazefaceback.pth'))
+        blazeface.load_anchors(join(DATA_BLAZEFACE, 'anchorsback.npy'))
+
+    print('blazeface: %s model loaded with %s.' % (model, device))
+    model_cached[key] = blazeface
+    return blazeface
+
+@app.post('/blazeface/prepare')
+async def blazeface (cuda: Optional[int] = None, model: str = Query('front', enum=['front', 'back'])):
+    start = perf_counter()
+    load_model(model, cudaDevice(cuda))
+    return {
+        'duration': perf_counter() - start,
+    }
 
 @app.post('/blazeface')
-async def blazeface (cuda: Optional[int], file: UploadFile):
+async def blazeface (file: UploadFile, cuda: Optional[int] = None, model: str = Query('front', enum=['front', 'back'])):
     cuda = cudaDevice(cuda)
+    front_net = load_model(model, cuda)
+
     buffer = await file.read()
     img = cv2.imdecode(np.frombuffer(buffer, dtype=np.uint8), flags=cv2.IMREAD_COLOR)
     height, width = img.shape[:2]
@@ -21,31 +53,21 @@ async def blazeface (cuda: Optional[int], file: UploadFile):
 
     img = cv2.copyMakeBorder(img, top, size - height - top, left, size - width - left, cv2.BORDER_CONSTANT, (0, 0, 0))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (256, 256))
-    img128 = cv2.resize(img, (128, 128))
 
-    print('blazeface: image loaded.')
+    if model == 'front':
+        img = cv2.resize(img, (128, 128))
 
-    front_net = BlazeFace().to(cuda)
-    front_net.load_weights(join(DATA_BLAZEFACE, 'blazeface.pth'))
-    front_net.load_anchors(join(DATA_BLAZEFACE, 'anchors.npy'))
-
-    print('blazeface: front model loaded.')
-
-    # back_net = BlazeFace(back_model=True).to(cuda)
-    # back_net.load_weights(join(DATA_BLAZEFACE, 'blazefaceback.pth'))
-    # back_net.load_anchors(join(DATA_BLAZEFACE, 'anchorsback.npy'))
-
-    # print('blazeface: back model loaded.')
+    if model == 'back':
+        img = cv2.resize(img, (256, 256))
 
     # front_net.min_score_thresh = 0.75
     # front_net.min_suppression_threshold = 0.3
 
-    front_detections = front_net.predict_on_image(img128)
+    front_detections = front_net.predict_on_image(img)
     front_detections = front_detections.cpu().numpy()
 
     return {
-        'front': [
+        'faces': [
             {
                 'top': face[0] * size - top,
                 'left': face[1] * size - left,
